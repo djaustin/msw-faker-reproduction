f90f40826629c30fed4783b58eebe4dabd20077f
Dan Austin <dan.austin@hey.com>
2022-05-04 15:14:17 +0100

    add faker to model

--- mocks/model/db.js
+++ mocks/model/db.js
@@ -1,10 +1,10 @@
 import { factory, primaryKey } from "@mswjs/data";
+import { faker } from "@faker-js/faker";
 
 export const db = factory({
   book: {
     id: primaryKey(() => Math.floor(Math.random() * 9999)),
-    title: () => "Book title",
-    imageUrl: () =>
-      "https://images.dog.ceo/breeds/terrier-toy/n02087046_7469.jpg",
+    title: faker.commerce.productName,
+    imageUrl: faker.image.image(),
   },
 });
